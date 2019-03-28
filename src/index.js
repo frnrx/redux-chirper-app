74d312098cc3ccacfb2846d4004a73ff41bd7db1
Gabriela Froener <gabriela.froener@gmail.com>
2019-03-28 13:35:06 -0300

    applying middleware and loggin all the inital data

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import App from './components/App'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
+import middleware from './middleware' //it imports the default of index.js inside of /middleware (that is the applyMiddleware invocation)
 
-const store = createStore(reducer)
+
+const store = createStore(reducer, middleware)
 
 ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>, 
-    document.getElementById('root'))
+    document.getElementById('root')
+)
