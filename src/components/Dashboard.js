2d5478ac63d21e15ed12946c8e16291b8066e8e5
Gabriela Froener <gabriela.froener@gmail.com>
2019-03-30 09:55:31 -0300

    makinig the tweet page component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import Tweet from './Tweets'
+import Tweet from './Tweet'
 
 class Dashboard extends Component {
   render() {
